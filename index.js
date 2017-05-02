cbc592c73675893257e9d360e074b122524e710a
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-05-02 11:06:15 +0200

    add some cpu consuming task

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var os = require('os');
 var bodyParser = require('body-parser');
+var crypto = require('crypto');
 
 // log reqs
-app.use(function log (req, res, next) {
+app.use(function log(req, res, next) {
   console.log([req.method, req.url].join(' '));
   next();
 });
@@ -14,16 +15,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'jade');
 app.get('/', function(req, res) {
-    res.render("index",
-               { 
-                 version: '2.0.0',
-                 agent: req.headers['user-agent'],
-                 hostname: os.hostname(),
-                 os: os.type(),
-                 nodeversion: process.version
-               });
+  crypto.pbkdf2('secret', 'salt', 100000, 512, 'sha512', function(err, key) {
+    if (err) { throw err; }
+    console.log(key.toString('hex')); 
+  });
+
+  res.render("index", {
+    version: '2.0.0',
+    agent: req.headers['user-agent'],
+    hostname: os.hostname(),
+    os: os.type(),
+    nodeversion: process.version
+  });
 });
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log('Listening on %s', process.env.PORT || 3000);
+  console.log('Listening on %s', process.env.PORT || 3000);
 });
