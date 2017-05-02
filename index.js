c5a5bc799e1ab2323d52fe7b8b2d2c1b9ccdf8a0
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-05-02 10:55:09 +0200

    demo app version

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 var os = require('os');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 
 // log reqs
 app.use(function log (req, res, next) {
@@ -17,13 +15,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'jade');
 app.get('/', function(req, res) {
     res.render("index",
-               { agent: req.headers['user-agent'],
+               { 
+                 version: '2.0.0',
+                 agent: req.headers['user-agent'],
                  hostname: os.hostname(),
                  os: os.type(),
                  nodeversion: process.version
                });
 });
 
-var server = app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on %s', process.env.PORT || 3000);
 });
